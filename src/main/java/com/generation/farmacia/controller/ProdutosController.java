af0aa6cdc747f615df5ff5187166d5b7736aadfa
Giovanni Santos <127205710+AzvalSG@users.noreply.github.com>
2023-04-20 11:07:46 -0300

    Add files via upload
    Atualização de funcionalidade

--- src/main/java/com/generation/farmacia/controller/ProdutosController.java
+++ src/main/java/com/generation/farmacia/controller/ProdutosController.java
@@ -30,18 +30,18 @@ public class ProdutosController {
 
 	@Autowired
 	private ProdutosRepository produtosRepository;
-
+//Retorna todos os produtos cadastrados até o momento.
 	@GetMapping
 	public ResponseEntity<List<Produtos>> getAll() {
 		return ResponseEntity.ok(produtosRepository.findAll());
 	}
-
+//Adiciona a busca por ID
 	@GetMapping("/{id}")
 	public ResponseEntity<Produtos> getById(@PathVariable Long id) {
 		return produtosRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
-
+//Adiciona a busca por nome
 	@GetMapping("nomeprod / {nomeprod}")
 	public ResponseEntity<List<Produtos>> getByNomeprod(@PathVariable String nomeprod) {
 		return ResponseEntity.ok(produtosRepository.findAllByNomeprodContainingIgnoreCase(nomeprod));
@@ -51,14 +51,14 @@ public class ProdutosController {
 	public ResponseEntity<Produtos> post(@Valid @RequestBody Produtos produtos) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(produtosRepository.save(produtos));
 	}
-
+//Altera um produto já cadastrado
 	@PutMapping
 	public ResponseEntity<Produtos> put(@Valid @RequestBody Produtos produtos) {
 		return produtosRepository.findById(produtos.getId())
 				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(produtosRepository.save(produtos)))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
-
+//Deleta um produto, caso ele não exista retorna o status not found
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable Long id) {
